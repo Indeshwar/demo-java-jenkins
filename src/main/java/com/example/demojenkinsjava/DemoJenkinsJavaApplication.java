e7ef517031b415bae3725ed61ae8b86b7433c3ba
Indeshwar <indeshc@gmail.com>
2023-06-26 19:10:23 -0500

    Create Project

---
+++ src/main/java/com/example/demojenkinsjava/DemoJenkinsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojenkinsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demojenkinsjava/DemoJenkinsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojenkinsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
